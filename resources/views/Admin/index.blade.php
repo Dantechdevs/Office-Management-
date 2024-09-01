d77d0e3ba894820067d9ce2f1a8046d746137087
dantechdevs <damnngwasi@gmail.com>
2024-09-01 21:14:09 +0300

    template segmentation

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -1,10 +1,11 @@
+
 @extends('admin.admin_dashboard')
 @section('admin')
 <div class="page-content">
 
     <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
       <div>
-        <h4 class="mb-3 mb-md-0">NZAUI SUB COUNTY</h4>
+        <h4 class="mb-3 mb-md-0">REAL ESTATE MANAGEMENT DASHBOARD</h4>
       </div>
       <div class="d-flex align-items-center flex-wrap text-nowrap">
         <div class="input-group flatpickr wd-200 me-2 mb-2 mb-md-0" id="dashboardDate">
