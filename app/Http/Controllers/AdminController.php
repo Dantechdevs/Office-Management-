1baa3072e4039fdb1c1d3b2f18e9e4a0b5ebdf70
dantechdevs <damnngwasi@gmail.com>
2024-09-07 22:10:03 +0300

    changing photo

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -53,6 +53,7 @@ public function AdminProfileStore(Request $request)
 
     if ($request->file('photo')){
       $file= $request->file('photo');
+      @unlink(public_path('upload/admin_images/'.$data->photo));
       $filename=date('YmdHi').$file->getClientOriginalName();
       $file->move(public_path('upload/admin_images'),$filename);
       $data['photo'] = $filename;
--- resources/views/Admin/partials/navbar.blade.php
+++ resources/views/Admin/partials/navbar.blade.php
@@ -191,7 +191,10 @@
                 </div>
             </li>
 
-
+        @php
+    $id = Auth::user()->id;
+    $profileData = App\Models\User::find($id);
+        @endphp
 
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -204,8 +207,9 @@
                  <img class="wd-80 ht-80 rounded-circle" src="{{ !empty($profileData->photo) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="">
              </div>
              <div class="text-center">
-
-             </div>
+                <p class="tx-16 fw-bolder">{{ $profileData->name }}</p>
+               <p class="tx-12 text-muted">{{ $profileData->email }}</p>
+            </div>
 
             </div>
     <ul class="list-unstyled p-1">
