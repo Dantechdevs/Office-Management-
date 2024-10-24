9fb68e6a0e3fc692dd1203ffb6da419710998c9c
dantechdevs <damnngwasi@gmail.com>
2024-10-24 11:57:59 +0300

    update

--- resources/views/Admin/partials/sidebar.blade.php
+++ resources/views/Admin/partials/sidebar.blade.php
@@ -62,106 +62,30 @@
             <li class="nav-item">
                 <a class="nav-link" data-bs-toggle="collapse" href="#uiComponents" role="button" aria-expanded="false" aria-controls="uiComponents">
                     <i class="link-icon" data-feather="monitor"></i>
-                    <span class="link-title">DLP</span>
+                    <span class="link-title">DIGITAL LITERACY(DLP)</span>
                     <i class="link-arrow" data-feather="chevron-down"></i>
                 </a>
                 <div class="collapse" id="uiComponents">
                     <ul class="nav sub-menu">
                         <li class="nav-item">
-                            <a href="pages/ui-components/accordion.html" class="nav-link">Accordion</a>
+                            <a href="pages/ui-components/accordion.html" class="nav-link">Learner Devices(LDD)</a>
                         </li>
                         <li class="nav-item">
-                            <a href="pages/ui-components/alerts.html" class="nav-link">Alerts</a>
+                            <a href="pages/ui-components/alerts.html" class="nav-link">Teacher Devices(TDD)</a>
                         </li>
                         <li class="nav-item">
-                            <a href="pages/ui-components/badges.html" class="nav-link">Badges</a>
+                            <a href="pages/ui-components/badges.html" class="nav-link">Routers</a>
                         </li>
                         <li class="nav-item">
-                            <a href="pages/ui-components/breadcrumbs.html" class="nav-link">Breadcrumbs</a>
+                            <a href="pages/ui-components/breadcrumbs.html" class="nav-link">Hard Disks</a>
                         </li>
                         <li class="nav-item">
-                            <a href="pages/ui-components/buttons.html" class="nav-link">Buttons</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/button-group.html" class="nav-link">Button group</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/cards.html" class="nav-link">Cards</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/carousel.html" class="nav-link">Carousel</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/collapse.html" class="nav-link">Collapse</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/dropdowns.html" class="nav-link">Dropdowns</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/list-group.html" class="nav-link">List group</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/media-object.html" class="nav-link">Media object</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/modal.html" class="nav-link">Modal</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/navs.html" class="nav-link">Navs</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/navbar.html" class="nav-link">Navbar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/pagination.html" class="nav-link">Pagination</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/popover.html" class="nav-link">Popovers</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/progress.html" class="nav-link">Progress</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/scrollbar.html" class="nav-link">Scrollbar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/scrollspy.html" class="nav-link">Scrollspy</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/spinners.html" class="nav-link">Spinners</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/tabs.html" class="nav-link">Tabs</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/ui-components/tooltips.html" class="nav-link">Tooltips</a>
+                            <a href="pages/ui-components/buttons.html" class="nav-link">Projectors</a>
                         </li>
                     </ul>
                 </div>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#advancedUI" role="button" aria-expanded="false" aria-controls="advancedUI">
-                    <i class="link-icon" data-feather="anchor"></i>
-                    <span class="link-title">Advanced UI</span>
-                    <i class="link-arrow" data-feather="chevron-down"></i>
-                </a>
-                <div class="collapse" id="advancedUI">
-                    <ul class="nav sub-menu">
-                        <li class="nav-item">
-                            <a href="pages/advanced-ui/cropper.html" class="nav-link">Cropper</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/advanced-ui/owl-carousel.html" class="nav-link">Owl carousel</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/advanced-ui/sortablejs.html" class="nav-link">SortableJs</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/advanced-ui/sweet-alert.html" class="nav-link">Sweet Alert</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
+
             <li class="nav-item">
                 <a class="nav-link" data-bs-toggle="collapse" href="#forms" role="button" aria-expanded="false" aria-controls="forms">
                     <i class="link-icon" data-feather="inbox"></i>
@@ -255,7 +179,7 @@
             <li class="nav-item">
                 <a class="nav-link" data-bs-toggle="collapse" href="#general-pages" role="button" aria-expanded="false" aria-controls="general-pages">
                     <i class="link-icon" data-feather="book"></i>
-                    <span class="link-title">Audited Schools</span>
+                    <span class="link-title">RECORDS</span>
                     <i class="link-arrow" data-feather="chevron-down"></i>
                 </a>
                 <div class="collapse" id="general-pages">
