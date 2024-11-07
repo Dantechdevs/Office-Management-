f31747afebf58716582f444511c073f5289f2892
dantechdevs <damnngwasi@gmail.com>
2024-11-07 09:34:37 +0300

    updated dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,7 +13,7 @@ class AdminController extends Controller
 {
     public function AdminDashboard()
     {
-       return view('admin.admin_dashboard');
+       return view('Admin.index');
     } //End Method
 
 
