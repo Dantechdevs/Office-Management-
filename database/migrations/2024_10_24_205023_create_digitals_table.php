38da8ab8f725371fa937eae2b54cd79c0ddb076e
dantechdevs <damnngwasi@gmail.com>
2024-11-16 07:47:34 +0300

    updated forms

--- database/migrations/2024_10_24_205023_create_digitals_table.php
+++ database/migrations/2024_10_24_205023_create_digitals_table.php
@@ -12,10 +12,22 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('digitals', function (Blueprint $table) {
-            $table->id();
-            $table->string('device_type'); // To store the type of device
-            $table->string('description')->nullable(); // Optional description
-            $table->timestamps();
+
+            $table->id(); // Auto-incrementing ID
+            $table->string('sn')->unique(); // Serial Number
+            $table->string('county'); // County
+            $table->string('sub_county'); // Sub County
+            $table->string('zone'); // Zone
+            $table->string('school_name'); // School Name
+            $table->date('date_of_delivery'); // Date of Delivery
+            $table->integer('teacher_devices')->default(0); // Teacher Devices
+            $table->integer('learner_devices')->default(0); // Learner Devices
+            $table->integer('hard_disk_drives')->default(0); // Hard Disk Drives
+            $table->integer('router')->default(0); // Router
+            $table->integer('projector')->default(0); // Projector
+            $table->string('device_type'); // Add this line for device type
+            $table->timestamps(); // Created at and Updated at timestamps
+
         });
     }
 
